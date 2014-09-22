afa5a6a8bfd5e5b67fed27abb14665702fefdbd8
dprotaso <dprotaso@gmail.com>
2014-09-22 12:51:07 -0700

    Have the batch script build the project prior to staging the data

--- Google.PhoneNumbers.GenerateData/StageData.bat
+++ Google.PhoneNumbers.GenerateData/StageData.bat
@@ -1,4 +1,11 @@
-﻿
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild" ^
+  "%cd%\..\Google.PhoneNumbers.sln" ^
+  /t:Clean ^
+  /t:Build ^
+  /p:Configuration=StageData ^
+  /verbosity:minimal
+
+if ERRORLEVEL 1 Exit /B 1
 
 .\bin\StageData\Google.PhoneNumbers.GenerateData.exe ^
   --input-file=%cd%\Data\PhoneNumberMetadata.xml ^
@@ -8,6 +15,8 @@
   --copyright=2014 ^
   --lite-build=false
 
+if ERRORLEVEL 1 Exit /B 1
+
 .\bin\StageData\Google.PhoneNumbers.GenerateData.exe ^
   --input-file=%cd%\Data\PhoneNumberAlternateFormats.xml ^
   --output-dir=%cd%\..\Google.PhoneNumbers ^
@@ -16,6 +25,8 @@
   --copyright=2014 ^
   --lite-build=false
 
+if ERRORLEVEL 1 Exit /B 1
+
 .\bin\StageData\Google.PhoneNumbers.GenerateData.exe ^
   --input-file=%cd%\Data\ShortNumberMetadata.xml ^
   --output-dir=%cd%\..\Google.PhoneNumbers ^
@@ -23,6 +34,8 @@
   --mapping-class=ShortNumbersRegionCodeSet ^
   --copyright=2014 ^
   --lite-build=false
+
+if ERRORLEVEL 1 Exit /B 1
 
 .\bin\StageData\Google.PhoneNumbers.GenerateData.exe ^
   --input-file=%cd%\Data\PhoneNumberMetadataForTesting.xml ^
